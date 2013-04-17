375f19e01e6df3c448ccc559f212369548856761
harri_k <harri.kinnunen@hitsaamo.fi>
2013-04-17 18:43:33 +0300

    setup

---
+++ conn_test.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+console.log('Request');
+}).listen(8080, '0.0.0.0');
