2284618e6eb6d6abcf2c5bd054a7d1259451e45b
harri_k <harri.kinnunen@hitsaamo.fi>
2013-11-03 00:48:33 +0200

    putzen

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.listen(8080);
+/*
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+*/
